f8c7395aed246e77909d65eab5d128d94cb066b7
tomamang-cmd <tomamang@gmail.com>
2026-09-25 08:45:13 +0800

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
-  url: 'https://laruorruyjjchilqmcmb.supabase.co/',
+  url: 'https://laruorruyjjchilqmcmb.supabase.co',
   key: 'sb_publishable_rJ4kwCf_Or6hICjMi1DRYw_k6Y6Q1Jh'
 };
